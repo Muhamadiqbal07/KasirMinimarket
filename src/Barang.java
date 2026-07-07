12f3fc9d9df44692cd955e136a47b39640f08261
Muhamadiqbal07 <84624980+Muhamadiqbal07@users.noreply.github.com>
2026-07-07 17:16:24 +0700

    Update Barang.java

--- src/Barang.java
+++ src/Barang.java
@@ -1,12 +1,13 @@
 package KasirMinimarket;
 
+// Deklarasi class barang
 public class Barang {
+    private String kodeBarang;  // Menyimpan kode barang
+    private String namaBarang;  // Menyimpan nama barang
+    private double harga;       // Menyimpan harga barang
+    private int qty;            // Menyimpan kuantitas
 
-    private String kodeBarang;
-    private String namaBarang;
-    private double harga;
-    private int qty;
-
+    // Constructor barang untuk inisialisasi data barang
     public Barang(String kodeBarang, String namaBarang, double harga, int qty) {
         this.kodeBarang = kodeBarang;
         this.namaBarang = namaBarang;
@@ -14,22 +15,27 @@ public class Barang {
         this.qty = qty;
     }
 
+    // Getter kode barang
     public String getKodeBarang() {
         return kodeBarang;
     }
 
+    // Getter nama barang
     public String getNamaBarang() {
         return namaBarang;
     }
 
+    // Getter harga
     public double getHarga() {
         return harga;
     }
 
+    // Getter kuantitas
     public int getQty() {
         return qty;
     }
 
+    // Method getSubtotal() untuk menghitung dan mengembalikan nilai subtotal
     public double getSubtotal() {
         return harga * qty;
     }
